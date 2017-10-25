8207255f74eaa0abf918086618a5d5c4804fa13a
pedrojpaez <pedrojpaez@gmail.com>
2017-10-25 23:09:43 +0200

    https port for callbackURL google auth

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
         'googleAuth' : {
             'clientID'      : '99030273589-9gmh7h2vc5jgjtlnm2de981lbvvndgg6.apps.googleusercontent.com',
             'clientSecret'  : '0tftrfHJtsxq7j7ICidq06Me',
-            'callbackURL'   : 'http://webapp-183214.appspot.com/auth/google/callback'
+            'callbackURL'   : 'https://webapp-183214.appspot.com/auth/google/callback'
         }
     
     };
