5f4070ff6e87df58383113335409cf0538e7687c
pedrojpaez <pedrojpaez@gmail.com>
2017-10-25 22:52:51 +0200

    fix callbackURL for google auth

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
         'googleAuth' : {
             'clientID'      : '99030273589-9gmh7h2vc5jgjtlnm2de981lbvvndgg6.apps.googleusercontent.com',
             'clientSecret'  : '0tftrfHJtsxq7j7ICidq06Me',
-            'callbackURL'   : 'http://127.0.0.1:8080/auth/google/callback'
+            'callbackURL'   : 'http://webapp-183214.appspot.com/auth/google/callback'
         }
     
     };
