c7230776ab7f9490217ac21ddff8041707861fa5
pedrojpaez <pedrojpaez@gmail.com>
2017-10-26 10:32:02 +0200

    Change profile page

--- app/routes.js
+++ app/routes.js
@@ -78,11 +78,11 @@ module.exports = function(app, passport) {
         app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
         
             // the callback after google has authenticated the user
-            app.get('/auth/google/callback',
+        app.get('/auth/google/callback',
                     passport.authenticate('google', {
                             successRedirect : '/profile',
                             failureRedirect : '/'
-                    }));
+        }));
     
     };
     
